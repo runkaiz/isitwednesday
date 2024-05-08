3ee309ffe099be1a287d08dcd5db2302df23ff1b
Runkai Zhang <rzhang04@proton.me>
2024-05-08 14:36:25 -0700

    Add api

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,12 +3,12 @@ import { describe, it, expect } from 'vitest';
 
 describe('wednesday test', () => {
 	it('sees if the given timestamp is on a wednesday', () => {
-		expect(isItWednesday(1714628724)).toBe(true);
+		expect(isItWednesday(1714628724000)).toBe(true);
 	});
 });
 
 describe('not wednesday test', () => {
 	it('sees if the given timestamp is not on a wednesday', () => {
-		expect(isItWednesday(1714715124)).toBe(false);
+		expect(isItWednesday(1714715124000)).toBe(false);
 	});
 });
---
+++ src/routes/api/really/+server.ts
@@ -0,0 +1,14 @@
+import { error, type RequestHandler } from "@sveltejs/kit";
+import { isItWednesday, getSnark } from '$lib/helper';
+
+export const GET: RequestHandler = ({ url }) => {
+	const t = Number(url.searchParams.get('time') ?? '0');
+
+    if (isNaN(t) && t > 0) {
+        const w = isItWednesday(t);
+        const s = getSnark(w);
+        return new Response(JSON.stringify({ wednesday: w, msg: s}));
+    } else {
+        error(400, 'Hmmm, a traveler from beyond time and space?');
+    }
+};
