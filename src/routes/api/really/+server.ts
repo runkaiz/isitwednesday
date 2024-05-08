6816cfd382ca23d769ff4e77bb32041fae734816
Runkai Zhang <rzhang04@proton.me>
2024-05-08 14:46:10 -0700

    Fix

--- src/routes/api/really/+server.ts
+++ src/routes/api/really/+server.ts
@@ -4,10 +4,11 @@ import { isItWednesday, getSnark } from '$lib/helper';
 export const GET: RequestHandler = ({ url }) => {
 	const t = Number(url.searchParams.get('time') ?? '0');
 
-    if (isNaN(t) && t > 0) {
+    if (!isNaN(t) && t > 0) {
         const w = isItWednesday(t);
         const s = getSnark(w);
-        return new Response(JSON.stringify({ wednesday: w, msg: s}));
+
+        return new Response(JSON.stringify({ wednesday: w, msg: s, time: String(new Date(t))}));
     } else {
         error(400, 'Hmmm, a traveler from beyond time and space?');
     }
